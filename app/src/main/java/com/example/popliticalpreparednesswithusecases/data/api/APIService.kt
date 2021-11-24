fdb6309032ad4d767f9cd2871967be952b4c07c8
paul77uk <paul77uk@gmail.com>
2021-11-24 15:22:52 +0000

    11th commit - errors in representative fragment and representative adapter stopping it compiling

--- app/src/main/java/com/example/popliticalpreparednesswithusecases/data/api/APIService.kt
+++ app/src/main/java/com/example/popliticalpreparednesswithusecases/data/api/APIService.kt
@@ -1,15 +1,13 @@
 package com.example.popliticalpreparednesswithusecases.data.api
 
-import com.example.popliticalpreparednesswithusecases.BuildConfig
 import com.example.popliticalpreparednesswithusecases.data.model.ElectionResponse
-import com.example.popliticalpreparednesswithusecases.data.model.Representative
 import com.example.popliticalpreparednesswithusecases.data.model.RepresentativeResponse
 import com.example.popliticalpreparednesswithusecases.data.model.VoterInfoResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val MY_KEY="AIzaSyDkg-YrAFUANAgMZKUX3GBIqBG4KOHj-nw"
+const val MY_KEY = "AIzaSyDkg-YrAFUANAgMZKUX3GBIqBG4KOHj-nw"
 
 interface APIService {
 
